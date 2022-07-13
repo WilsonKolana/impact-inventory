369ef8f50f3b2ed27314d6c3ab64c1beac2fed8f
wmakoae <wmakoae>
2022-07-13 13:50:18 +0200

    Added some routes the app user pages

--- ClientApp/src/app/app-routing.module.ts
+++ ClientApp/src/app/app-routing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { UserComponent } from './user/user.component';
+import { RegistrationComponent } from './user/registration/registration.component';
+import { RouterModule, Routes } from '@angular/router';
 
-
+const routes: Routes = [
+  {path:'', redirectTo:'user/registration', pathMatch:'full'},
+  {
+    path:'user', component: UserComponent,
+    children: [
+      {path: 'registration', component: RegistrationComponent}
+    ]
+  }
+]
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
