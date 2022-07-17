d2beb842f2f8a063f7e3cd46e66150c91b42e0a5
wilson <makoaewilson@gmail.com>
2022-07-17 10:43:56 +0200

    change the product form to the ngmodel instead

--- ClientApp/src/app/home/product-report-form/product-report-form.component.ts
+++ ClientApp/src/app/home/product-report-form/product-report-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ProductReportService } from 'src/app/services/product-report.service';
 
 @Component({
   selector: 'app-product-report-form',
@@ -8,9 +10,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductReportFormComponent implements OnInit {
 
-  constructor() { }
+  constructor(public service: ProductReportService) { }
 
   ngOnInit(): void {
+  }
+
+  resertForm(form: NgForm){
+    form.form.reset();
   }
 
 }
